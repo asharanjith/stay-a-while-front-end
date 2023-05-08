698c06a0342c94aa1f3335896177811f51a8264e
admirerbrown <admirerbrown@gmail.com>
2023-05-08 15:54:45 +0000

    Add nav routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/navbar/Navbar';
+import Navbarlist from './components/navbarlist/Navbarlist';
 import Home from './components/home/Home';
 import Booking from './components/booking/Booking';
+import Reservation from './components/reservation/Reservation';
+import Detailed from './components/home/detailed/Detailed';
+import AddHotel from './components/add_hotel/AddHotel';
+import DeleteHotel from './components/delete_hotel/DeleteHotel';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbarlist />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/detailed/:id" element={<Detailed />} />
         <Route path="/booking" element={<Booking />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/add" element={<AddHotel />} />
+        <Route path="/delete" element={<DeleteHotel />} />
       </Routes>
     </div>
   );
 }
-
 export default App;
