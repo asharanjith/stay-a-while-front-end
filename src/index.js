97c3c0aa2d1f13c6dcde4c4373ebefed560570ef
admirerbrown <admirerbrown@gmail.com>
2023-05-08 15:15:31 +0000

    Add redux store setup to app

--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@ import Popper from 'popper.js';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 
 import './index.css';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './components/configureStore';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+
 );
