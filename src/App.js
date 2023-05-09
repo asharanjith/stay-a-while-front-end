6fe04c8a32babefaaade558cf37333b205714fc2
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-09 23:08:12 +0530

    update app for login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbarlist from './components/navbarlist/Navbarlist';
 import Home from './components/home/Home';
 import Booking from './components/booking/Booking';
@@ -9,19 +10,37 @@ import Detailed from './components/home/detailed/Detailed';
 import AddHome from './components/add_home/AddHome';
 import DeleteHome from './components/delete_home/DeleteHome';
 import Login from './components/login/Login';
+import { loginset } from './components/login/loginSlicer';
+import Signup from './components/signup/Signup';
 
 function App() {
+  const { login } = useSelector((state) => state.login);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      dispatch(loginset(token));
+    }
+  }, [dispatch]);
   return (
     <div className="App">
       <Navbarlist />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/detailed/:id" element={<Detailed />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/reservation" element={<Reservation />} />
-        <Route path="/add" element={<AddHome />} />
-        <Route path="/delete" element={<DeleteHome />} />
+        {login ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/detailed/:id" element={<Detailed />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/reservation" element={<Reservation />} />
+            <Route path="/add" element={<AddHome />} />
+            <Route path="/delete" element={<DeleteHome />} />
+          </>
+        ) : (
+          <>
+            <Route path="/register" element={<Signup />} />
+            <Route path="*" element={<Login />} />
+          </>
+        )}
       </Routes>
     </div>
   );
