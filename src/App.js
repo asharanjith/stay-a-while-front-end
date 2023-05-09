96c6504ecf4be237dcde905e6a2c97535f70850e
admirerbrown <admirerbrown@gmail.com>
2023-05-09 10:44:14 +0000

    get latest changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import Booking from './components/booking/Booking';
+import { fetchHomeStays } from './components/home/HomeSlice';
 
 function App() {
   return (
+
     <div className="App">
       <Navbar />
       <Routes>
--- src/components/home/HomeSlice.js
+++ src/components/home/HomeSlice.js
@@ -1,36 +1,41 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const DEFAULTDATA = 'stayAwhile/DEFAULTDATA';
+const FETCH_HOME_STAYS = 'stayAwhile/FETCH_HOME_STAYS';
 
-// get property listing data...
-const GetData = createAsyncThunk(DEFAULTDATA, async () => {
-  const options = {
-    method: 'GET',
-    url: 'http://127.0.0.1:3000/home_stays',
-  };
-  const response = await axios.request(options);
-  const propertyList = response.data;
-  return propertyList;
+const fetchHomeStays = createAsyncThunk(FETCH_HOME_STAYS, async () => {
+  try {
+    const options = {
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/home_stays',
+    };
+    const response = await axios.request(options);
+    const propertyList = response.data;
+    console.log(propertyList);
+    return propertyList;
+  } catch (error) {
+    throw new Error('Failed to fetch home stays');
+  }
 });
 
-// create slice...
 const homeSlice = createSlice({
   name: 'homeStayList',
   initialState: {
-    listing: [],
+    listings: [],
     isFulfilled: false,
+    error: null,
   },
-
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(GetData.fulfilled, (state, action) => {
+    builder.addCase(fetchHomeStays.fulfilled, (state, action) => {
       state.isFulfilled = true;
-      state.listing = action.payload;
+      state.listings = action.payload;
+    });
+    builder.addCase(fetchHomeStays.rejected, (state, action) => {
+      state.error = action.error.message;
     });
   },
-
 });
 
 export default homeSlice;
-export { GetData };
+export { fetchHomeStays };
