9e9b03f8a12d5d50b027597601b5113e289b84e1
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-09 23:08:41 +0530

    config store

--- src/components/configureStore.js
+++ src/components/configureStore.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import loginReducer from './login/loginSlicer';
+import signupReducer from './signup/signupSlicer';
 
 const store = configureStore({
   reducer: {
-    // add reducers here
+    login: loginReducer,
+    signup: signupReducer,
   },
 });
 
