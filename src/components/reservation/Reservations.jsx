14ba73bcb6b52a827f1e95e65efd2f1fe94e7af0
elarhadu <elarhadu@gmail.com>
2023-05-10 12:36:26 +0000

    Create a view page for reservation

--- src/components/reservation/Reservations.jsx
+++ src/components/reservation/Reservations.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ReservationItem from './ReservationItem';
 
 export default function Reservation() {
+  const { state } = useLocation();
+
+  if (!state || !state.reservation) {
+    return <div>No reservation found.</div>;
+  }
+
+  const { reservation } = state;
+  console.log(state);
+
   return (
-    <div>Reservation</div>
+    <div>
+      {reservation.map((reserve) => (
+        <ReservationItem
+          key={reserve.property}
+          property={reserve.property}
+          numberOfPersons={reserve.numberOfPersons}
+          startDate={reserve.startDate}
+          endDate={reserve.endDate}
+        />
+      ))}
+    </div>
   );
 }
