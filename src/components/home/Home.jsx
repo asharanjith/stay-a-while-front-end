8143cd2250a08e68563eeacfae442039a89544c3
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-13 03:49:35 +0530

    add pagination

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,107 +1,93 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
 import { BsFillStarFill } from 'react-icons/bs';
+import ReactPaginate from 'react-paginate';
 import { fetchHomeStays } from './HomeSlice';
+import style from './Home.module.css';
 
 const Home = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const token = localStorage.getItem('token');
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const itemsPerPage = 6;
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const homeStayList = useSelector((state) => state.home.listings);
 
   useEffect(() => {
     dispatch(fetchHomeStays(token));
   }, [dispatch, token]);
+  const handlePageChange = (selectedPage) => {
+    setCurrentPage(selectedPage.selected);
+  };
 
-  const homeStayList = useSelector((state) => state.home.listings);
+  useEffect(() => {
+    if (homeStayList) {
+      setTotalPages(Math.ceil(homeStayList.length / itemsPerPage));
+    }
+  }, [homeStayList]);
+
   const errMsg = useSelector((state) => state.home.error);
+  const subset = homeStayList.slice(startIndex, endIndex);
 
   const handleClick = (id) => {
     navigate(`/home/${id}`);
   };
 
-  const imgSlide = (homeStay) => (
-    <div
-      id={`carouselExampleCaptions-${homeStay.id}`}
-      className="carousel slide"
-    >
-      <div className="carousel-indicators">
-        {homeStay.images.map((img, index) => (
-          <button
-            key={uuidv4()}
-            type="button"
-            data-bs-target={`#carouselExampleCaptions-${homeStay.id}`}
-            data-bs-slide-to={index}
-            className={index === 0 ? 'active' : ''}
-            aria-current={index === 0 ? 'true' : ''}
-            aria-label={`Slide ${index + 1}`}
-          />
-        ))}
-      </div>
-      <div className="carousel-inner">
-        {homeStay.images.map((img, index) => (
-          <div
-            key={uuidv4()}
-            className={`carousel-item img-holder ${
-              index === 0 ? 'active' : ''
-            }`}
-          >
-            <img
-              src={img.url}
-              className="d-block w-100 img-tag"
-              alt={`Slide ${index + 1}`}
-              onClick={() => handleClick(homeStay.id)}
-              onKeyDown={() => {}}
-            />
-          </div>
-        ))}
-      </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target={`#carouselExampleCaptions-${homeStay.id}`}
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true" />
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target={`#carouselExampleCaptions-${homeStay.id}`}
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true" />
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
-  );
-
   if (homeStayList) {
     return (
-      <div className="container mt-5">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xlg-4 g-3">
-          {homeStayList.map((homeStay) => (
-            <div key={homeStay.id} className="col">
+      <div className={style.container}>
+        <div className={style.cardContainer}>
+          {subset.map((homeStay) => (
+            <div
+              key={homeStay.id}
+              onClick={() => handleClick(homeStay.id)}
+              onKeyDown={() => {}}
+              role="button"
+              tabIndex={homeStay.id}
+              className={style.card}
+            >
               <div className="card h-100">
                 <div className="card-body">
-                  {imgSlide({ id: homeStay.id, images: homeStay.images })}
+                  {homeStay.images.length > 0
+                  && (
+                  <img
+                    className={style.image}
+                    src={homeStay.images[0].url}
+                    alt={homeStay.name}
+                  />
+                  )}
                   <div className="d-flex justify-content-between mt-3">
                     <h4 className="f_size">{homeStay.location}</h4>
                     <h6 className="">
                       <BsFillStarFill />
                       {homeStay.rating}
-                      .0
                     </h6>
                   </div>
-                  <h5 className="hse_name fs-9">{homeStay.name}</h5>
+                  <h5 className="hse_name">{homeStay.name}</h5>
                 </div>
               </div>
             </div>
           ))}
         </div>
+        <ReactPaginate
+          previousLabel="Previous"
+          nextLabel="Next"
+          pageCount={totalPages}
+          onPageChange={handlePageChange}
+          containerClassName="pagination justify-content-center"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          activeClassName="active"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+        />
       </div>
     );
   }
