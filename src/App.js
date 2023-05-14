2737ece6989ddb70a4972c2da8fe16c01276faf6
elarhadu <elarhadu@gmail.com>
2023-05-14 16:36:32 +0000

    Modify files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
+import { AnimatePresence, motion } from 'framer-motion';
 import Navbarlist from './components/navbarlist/Navbarlist';
 import Home from './components/home/Home';
 import Booking from './components/booking/Booking';
@@ -10,12 +11,16 @@ import Detailed from './components/home/detailed/Detailed';
 import DeleteHome from './components/delete_home/DeleteHome';
 import AddHome from './components/add_home/AddHome';
 import Login from './components/login/Login';
-import { loginset, loginreset } from './components/login/loginSlicer';
+import {
+  loginset,
+  loginreset,
+  usernamereset,
+} from './components/login/loginSlicer';
 import Signup from './components/signup/Signup';
 import './components/home/home.css';
 
 function App() {
-  const { login } = useSelector((state) => state.login);
+  const { login, username } = useSelector((state) => state.login);
   const dispatch = useDispatch();
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -25,9 +30,31 @@ function App() {
       dispatch(loginreset());
     }
   }, [dispatch]);
+  useEffect(() => {
+    if (login) {
+      setTimeout(() => {
+        dispatch(usernamereset());
+      }, 2000);
+    }
+  }, [login, dispatch]);
   return (
     <div className="App">
       <Navbarlist />
+      <AnimatePresence>
+        {username && (
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -100 }}
+            className="welcome-message"
+          >
+            Welcome
+            {' '}
+            {username}
+            !
+          </motion.div>
+        )}
+      </AnimatePresence>
       <Routes>
         {login && (
           <>
@@ -41,7 +68,7 @@ function App() {
             <Route path="/delete" element={<DeleteHome />} />
           </>
         )}
-        {(login === false) ? (
+        {login === false ? (
           <>
             <Route path="/register" element={<Signup />} />
             <Route path="*" element={<Login />} />
