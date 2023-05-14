f7b97c6f7785cc948716de2f93ec63e13060993c
elarhadu <elarhadu@gmail.com>
2023-05-14 12:10:28 +0000

    Fix bugs in code

--- src/components/reservation/Reservations.jsx
+++ src/components/reservation/Reservations.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteReservation, getListReservations } from './reservationSlice';
 import './reservations.css';
+import { fetchHomeStays } from '../home/HomeSlice';
 
 export default function Reservation() {
   const dispatch = useDispatch();
   const token = localStorage.getItem('token');
   const { reservations } = useSelector((state) => state.reservation);
+  const homeStayList = useSelector((state) => state.home.listings);
+
   useEffect(() => {
     dispatch(getListReservations(token));
+    dispatch(fetchHomeStays(token));
   }, [dispatch, token]);
+
   const handleDelete = async (reservationId) => {
     const payload = {
       id: reservationId,
@@ -19,23 +23,21 @@ export default function Reservation() {
     dispatch(deleteReservation(payload));
   };
 
-  const homeStayList = useSelector((state) => state.home.listings);
-
   return (
     <div className="max-w-[1000px] m-auto">
       <h1 className="text-3xl mb-4 text-center">My Reservations</h1>
-      {reservations && reservations.length === 0 ? (
+      {reservations.length === 0 ? (
         <div className="flex flex-col items-center justify-center">
           <p className="text-center text-gray-500">You have no reservations yet.</p>
           <p className="text-center text-gray-500">Go to the home page and book a home.</p>
         </div>
       ) : (
-        <div>
-          {reservations && reservations.map((reservation) => {
+        <div className="card-container">
+          {reservations.map((reservation) => {
             const homeStay = homeStayList.find((h) => h.id === reservation.home_stay_id);
             if (!homeStay) return null;
             return (
-              <div className="card-container" key={reservation.id}>
+              <div key={reservation.id}>
                 <div className="card">
                   <div className="card__img">
                     <img
@@ -44,9 +46,9 @@ export default function Reservation() {
                     />
                   </div>
                   <div className="card__descr-wrapper">
-                    <p className="card__title">
+                    <h2 className="card__title">
                       {homeStay.name}
-                    </p>
+                    </h2>
                     <p className="card__descr">
                       <br />
                       <span>
@@ -59,7 +61,7 @@ export default function Reservation() {
                     </p>
                     <button
                       type="button"
-                      className="btn btn-danger text-[#313131] px-4 py-2 rounded-lg"
+                      className="btn btn-danger px-4 py-2 rounded-lg"
                       onClick={() => handleDelete(reservation.id)}
                     >
                       Cancel
@@ -69,28 +71,8 @@ export default function Reservation() {
               </div>
             );
           })}
-
         </div>
       )}
     </div>
   );
 }
-
-Reservation.propTypes = {
-  reservation: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    start_date: PropTypes.string.isRequired,
-    end_date: PropTypes.string.isRequired,
-    home: PropTypes.shape({
-      price: PropTypes.number.isRequired,
-    }).isRequired,
-    images: PropTypes.arrayOf(
-      PropTypes.shape({
-        url: PropTypes.string.isRequired,
-      }),
-    ).isRequired,
-  }).isRequired,
-};
