b64538ac449765a8cca99c8eff628a442fc14cf9
elarhadu <elarhadu@gmail.com>
2023-05-14 13:54:47 +0000

    Add daily rate and total price

--- src/components/reservation/Reservations.jsx
+++ src/components/reservation/Reservations.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteReservation, getListReservations } from './reservationSlice';
-import './reservations.css';
+import styles from './Reservations.module.css';
 import { fetchHomeStays } from '../home/HomeSlice';
 
 export default function Reservation() {
@@ -24,7 +24,7 @@ export default function Reservation() {
   };
 
   return (
-    <div className="max-w-[1000px] m-auto">
+    <div className="max-w-[1000px] m-auto" style={{ flex: 1 }}>
       <h1 className="text-3xl mb-4 text-center">My Reservations</h1>
       {reservations.length === 0 ? (
         <div className="flex flex-col items-center justify-center">
@@ -32,24 +32,41 @@ export default function Reservation() {
           <p className="text-center text-gray-500">Go to the home page and book a home.</p>
         </div>
       ) : (
-        <div className="card-container">
+        <div className={styles.card_container}>
           {reservations.map((reservation) => {
             const homeStay = homeStayList.find((h) => h.id === reservation.home_stay_id);
             if (!homeStay) return null;
             return (
               <div key={reservation.id}>
-                <div className="card">
-                  <div className="card__img">
+                <div className={styles.card}>
+                  <div className={styles.card__img}>
                     <img
                       src={homeStay.images && homeStay.images.length > 0 && homeStay.images[0].url}
                       alt={homeStay.name}
                     />
                   </div>
-                  <div className="card__descr-wrapper">
-                    <h2 className="card__title">
+                  <div className={styles.card__descr_wrapper}>
+                    <h2 className={styles.card__title}>
                       {homeStay.name}
                     </h2>
-                    <p className="card__descr">
+                    <p className={styles.price}>
+                      <span className={styles.total_price}>
+                        $
+                        {(homeStay.price
+                          * (new Date(reservation.end_date).getTime()
+                            - new Date(reservation.start_date).getTime())) / (1000 * 3600 * 24)}
+                      </span>
+                      {' '}
+                      {' '}
+                      |
+                      <span className={styles.price_rate}>
+                        $
+                        {homeStay.price}
+                        {' '}
+                        per day
+                      </span>
+                    </p>
+                    <p className={styles.card__descr}>
                       <br />
                       <span>
                         {reservation.start_date}
