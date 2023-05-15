2ae3c5122a5ecfeaec88cb6fa6c25622e3d9e3b0
asharanjith <ashasvijayan@gmail.com>
2023-05-15 07:25:47 +0400

    Fix test for reservation listing

--- src/tests/list_reservation/Reservations.test.js
+++ src/tests/list_reservation/Reservations.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { render, act, screen } from '@testing-library/react';
 import { configureStore } from '@reduxjs/toolkit';
-import axios from 'axios';
 import reservationSlice, { getListReservations } from '../../components/reservation/reservationSlice';
 import Reservations from '../../components/reservation/Reservations';
 import reservations from './reservations.json';
 import homeSlice, { fetchHomeStays } from '../../components/home/HomeSlice';
+import homestays from '../delete_homeStay/home_stays.json';
 
-jest.mock('axios');
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn(),
+}));
 
 const store = configureStore({
   reducer: {
@@ -21,10 +24,10 @@ const store = configureStore({
 
 describe('Reservations', () => {
   beforeEach(async () => {
-    axios.get.mockResolvedValue({
-      reservations: reservations.data.reservation,
-    });
-
+    useSelector.mockImplementation((selectorFn) => selectorFn({
+      reservation: { reservations: reservations.data.reservation },
+      home: { listings: homestays.data.home_stays },
+    }));
     await act(async () => {
       store.dispatch(getListReservations());
       store.dispatch(fetchHomeStays());
@@ -40,7 +43,7 @@ describe('Reservations', () => {
   });
 
   test('should have a submit button', () => {
-    expect(screen.getByRole('button', { name: /Cancel/i })).toBeInTheDocument();
+    expect(screen.getAllByRole('button', { name: /Cancel/i })).toHaveLength(2);
   });
 
   test('should match screenshot', () => {
@@ -51,11 +54,10 @@ describe('Reservations', () => {
         </Provider>,
       )
       .toJSON();
-
     expect(tree).toMatchSnapshot();
   });
 
   test('it should have an image of home stay', () => {
-    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getAllByRole('img')).toHaveLength(2);
   });
 });
